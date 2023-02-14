3f8c02a979a259716b462e30b48eb3876ef05323
thomas <hello@thomasflorentin.net>
2023-02-14 21:04:18 +0100

    styles: Polish publications

--- public/wp-content/themes/eschaton/components/blocs/bloc-publication.php
+++ public/wp-content/themes/eschaton/components/blocs/bloc-publication.php
@@ -4,29 +4,33 @@
 		<em><?php the_title(); ?></em>, <?php the_field('publication_author'); ?>
 	</h3>
 
-	<?php 
-		$term_obj_list = get_the_terms( $post->ID, 'publication_date' );
-		$terms_string = join(', ', wp_list_pluck($term_obj_list, 'name')); 
-		echo $terms_string;
-	?>
-	
-	<span class="separator"></span> 
-	
-	<?php 
-		$term_obj_list = get_the_terms( $post->ID, 'media_type' );
-		$terms_string = join(', ', wp_list_pluck($term_obj_list, 'name')); 
-		echo $terms_string;
-	?> 
-	
-	<span class="separator"></span> 
-	
-	<?php 
-		$term_obj_list = get_the_terms( $post->ID, 'publication_language' );
-		$terms_string = join(', ', wp_list_pluck($term_obj_list, 'name')); 
-		echo $terms_string;
-	?>
-	
-	<?php if( get_field('publication_isbn') !== '' && get_field('publication_isbn') ) : ?>
-		<div class="publication_isbn">isbn : <?php the_field('publication_isbn'); ?></div>
-	<?php endif; ?>
+	<p class="item_meta">
+		<?php 
+			$term_obj_list = get_the_terms( $post->ID, 'publication_date' );
+			$terms_string = join(', ', wp_list_pluck($term_obj_list, 'name')); 
+			echo $terms_string;
+		?>
+		
+		<span class="separator">-</span> 
+		
+		<?php 
+			$term_obj_list = get_the_terms( $post->ID, 'media_type' );
+			$terms_string = join(', ', wp_list_pluck($term_obj_list, 'name')); 
+			echo $terms_string;
+		?> 
+		
+		<span class="separator">-</span> 
+		
+		<?php 
+			$term_obj_list = get_the_terms( $post->ID, 'publication_language' );
+			$terms_string = join(', ', wp_list_pluck($term_obj_list, 'name')); 
+			echo $terms_string;
+		?>
+		
+		<?php if( get_field('publication_isbn') !== '' && get_field('publication_isbn') ) : ?>
+			<span class="separator"> - </span> 
+
+			<span class="publication_isbn">isbn : <?php the_field('publication_isbn'); ?></span>
+		<?php endif; ?>
+	</p>
 </div>
