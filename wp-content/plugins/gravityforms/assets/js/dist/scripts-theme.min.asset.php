8b09857c990d175880a749add884452186893bde
Thomas <hello@thomasflorentin.net>
2025-05-07 10:11:41 +0200

    updates: Gravity Forms Version 2.9.7.2

--- wp-content/plugins/gravityforms/assets/js/dist/scripts-theme.min.asset.php
+++ wp-content/plugins/gravityforms/assets/js/dist/scripts-theme.min.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('gform_gravityforms_utils', 'jquery', 'wp-polyfill'), 'version' => '5006ced855fca3149072');
+<?php return array('dependencies' => array('gform_gravityforms_utils', 'jquery', 'wp-polyfill'), 'version' => '432ac1965f9ee22a65a7');
